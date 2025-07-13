a5c1575b457088db0ee641242a07de6932c13c7a
ipitio <21136719+ipitio@users.noreply.github.com>
2025-07-13 01:29:31 -0400

    preserve last log

--- kickstart.sh
+++ kickstart.sh
@@ -4,6 +4,7 @@
 script_path="$(dirname "$(readlink -f "$0")")"
 sudo -i bash <<EOF
 pushd "$script_path" || exit 1
+[ ! -f start.log ] || mv start.log start.log.old 2>/dev/null
 CLS_WG_ONLY=${CLS_WG_ONLY:-false} bash start.sh ${@@Q} &>start.log
 popd || exit 1
 EOF
